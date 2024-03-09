33989270573ed4bb5cdc2e65dea7f02733aa22be
Guilherme de Freitas Lopes <155397702+oGuiFreitas@users.noreply.github.com>
2024-03-09 03:51:53 -0300

    Protótipo Login

--- App.js
+++ App.js
@@ -1,14 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'PlusJakartaSans': require('./assets/fonts/PlusJakartaSans-VariableFont_wght.ttf'),
+  });
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        style={{
+          width: 230,
+          height: 200,
+          resizeMode: 'contain',
+        }}
+        source={{
+          uri: './assets/logo.svg',
+        }}
+      />
+      <TextInput
+        style={[styles.input, styles.text]}
+        placeholder='Email'
+        placeholderTextColor='#581183'
+        keyboardType='email-address'
+      />
+       <TextInput
+        style={[styles.input, styles.text]}
+        placeholder='Senha'
+        placeholderTextColor='#581183'
+        selectionColor= '#581183'
+        textContentType= 'password' 
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={[styles.text, styles.textWhite]}>
+          Entrar
+        </Text>
+      </TouchableOpacity>
+    
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +49,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 20
+  },
+  text: {
+    fontFamily: 'PlusJakartaSans',
+    fontSize: 16,
+  },
+  input: {
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    fontWeight: '600',
+    borderWidth: 2,
+    borderColor: '#581183',
+    borderRadius: 15,
+    width: "80%"
+  },
+  button: {
+    color: '#581183',
+    backgroundColor: '#581183',
+    borderRadius: 20,
+    width: "70%",
+    paddingVertical:8,
+    alignItems: 'center',
+  },
+  textWhite: {
+    color:'#fff',
+    fontWeight: '600',
+    fontSize: 24
   },
 });
