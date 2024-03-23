241caa912ad8216c033613cf0bc8fc61c38f132c
Guilherme de Freitas Lopes <155397702+oGuiFreitas@users.noreply.github.com>
2024-03-22 21:59:54 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { useFonts } from 'expo-font';
 import { View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
-
+import { useState } from 'react';
 import styles from './style/Style.js';
+
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     'PlusJakartaSans': require('./assets/fonts/PlusJakartaSans-VariableFont_wght.ttf'),
   });
 
+  const [validation, setValidation] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function val() {
+    let campo1;
+    let campo2;
+    let validacao;
+
+    campo1 = { setEmail };
+    campo2 = { setPassword };
+
+    validacao = campo1, campo2;
+
+    setValidation(validacao);
+
+    if (email == "" || password == "") {
+      setValidation('Campo vazio')
+    }
+
+  }
 
   return (
     <View style={styles.container}>
@@ -21,34 +44,53 @@ export default function App() {
           uri: './assets/logo.svg',
         }}
       />
+
       <TextInput
         style={[styles.input, styles.text]}
         placeholder='Email'
         placeholderTextColor='#581183'
         keyboardType='email-address'
+        value={email}
+        onChangeText={text => setEmail(text)}
       />
+
+      <Text>
+        {validation}
+      </Text>
+
       <TextInput
+        value={password}
+        onChangeText={text => setPassword(text)}
         style={[styles.input, styles.text]}
+        secureTextEntry={true}
         placeholder='Senha'
         placeholderTextColor='#581183'
-        textContentType='password'
       />
-      <TouchableOpacity style={styles.button}>
+      <Text>
+        {validation}
+      </Text>
+
+      <TouchableOpacity
+        onPress={val}
+        style={styles.button}>
         <Text style={[styles.text, styles.textWhite]}>
           Entrar
         </Text>
       </TouchableOpacity>
+
+      {/*--- ou ---*/}
+
       <View style={styles.lineRow}>
         <View style={styles.line} />
         <View>
           <Text style={[styles.text, styles.textBold]}>ou</Text>
         </View>
         <View style={styles.line} />
-        
+
       </View>
 
-      <Text style={[styles.text]}>Ao continuar, você concorda com os Termos de Serviço e a Política de Privacidade.</Text>
-      <Text style={[styles.text]}>Ainda não esta no OldHelp? Crie sua conta.</Text>
+      <Text style={[styles.text, styles.textCenter]}>Ao continuar, você concorda com os Termos de Serviço e a Política de Privacidade.</Text>
+      <Text style={[styles.text, styles.textCenter]}>Ainda não esta no OldHelp? Crie sua conta.</Text>
 
 
     </View>
